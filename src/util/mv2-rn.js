33afa372ba21db5ff95818103546613a7e8eb8c4
Chris Greening <chris@cmgresearch.com>
2020-10-30 15:27:01 +0000

    Adds command marshalling between react-native and web Adds commands for saving and loading scratch files

--- src/util/mv2-rn.js
+++ src/util/mv2-rn.js
@@ -11,18 +11,13 @@ class Marty2 {
         this.power = 0;
         this.servos = 0;
         this.accel = 0;
-        this.savedProjectStates = {
-            '0': '',
-            '1': '',
-            '2': '',
-            '3': '',
-            '4': '',
-            '5': '',
-            '6': '',
-            '7': '',
-            '8': '',
-            '9': ''
-        };
+        this.commandPromise = null;
+        this.onCommandReply = this.onCommandReply.bind(this);
+        this.sendCommand = this.sendCommand.bind(this);
+        this.saveScratchFile = this.saveScratchFile.bind(this);
+        this.loadScratchFile = this.loadScratchFile.bind(this);
+        this.listSavedScratchFiles = this.listSavedScratchFiles.bind(this);
+        this.deleteScratchFile = this.deleteScratchFile.bind(this);
     }
 
     // eslint-disable-next-line camelcase
@@ -48,6 +43,82 @@ class Marty2 {
         } catch (err) {
             // eslint-disable-next-line no-console
             console.log(`Error sending to react native: ${err}`);
+        }
+    }
+
+    /**
+     * Save a scratch file
+     * @param string fileName Filename
+     * @param string contents Base64 data URL
+     */
+    saveScratchFile(fileName, contents) {
+        return this.sendCommand({
+            command: 'saveFile',
+            fileName,
+            contents
+        });
+    }
+
+    /**
+     * Delete a saved scratch file
+     * @param string fileName File to delete
+     */
+    deleteScratchFile(fileName) {
+        return this.sendCommand({
+            command: 'deleteFile',
+            fileName,
+        });
+    }
+
+    /**
+     * Load a scratch file
+     * @param string fileName File to load
+     */
+    loadScratchFile(fileName) {
+        return this.sendCommand({
+            command: 'loadFile',
+            fileName,
+        });
+    }
+
+    /**
+     * List the saved scratch files
+     */
+    listSavedScratchFiles() {
+        return this.sendCommand({
+            command: 'listFiles'
+        });
+    }
+
+    /**
+     * Sends a command to the react-native code and returns a promise that will be fulfilled when the react-native code replies
+     * @param {command: string} payload Payload to send to the react-native code
+     */
+    sendCommand(payload) {
+        if (this.commandPromise) {
+            console.warn("Command already in flight");
+        }
+        const promise = new Promise((resolve, reject) => {
+            this.commandPromise = { resolve, reject };
+        });
+        window.ReactNativeWebView.postMessage(JSON.stringify(payload));
+        return promise;
+    }
+
+    /**
+     * Called by the react-native code to respond to sendCommand
+     * @param {success: boolean, error?: string} args Response from the react native side
+     */
+    onCommandReply(args) {
+        if (this.commandPromise) {
+            if (args.success) {
+                this.commandPromise.resolve(args);
+            } else {
+                this.commandPromise.reject(new Error(args.error));
+            }
+            this.commandPromise = null;
+        } else {
+            console.warn("Unhandled command reply");
         }
     }
 
